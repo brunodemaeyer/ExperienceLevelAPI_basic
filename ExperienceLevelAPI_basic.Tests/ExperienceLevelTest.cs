dbb60c60672fab5fdaeef36ab59e018592481be3
Bruno De Maeyer <brunodemaeyer.it@gmail.com>
2018-03-04 15:42:22 +0100

    Class I wrote as a homework for a job application. Experience points to level gathering and conversion for a game.
    Add entire solution to GIT

---
+++ ExperienceLevelAPI_basic.Tests/ExperienceLevelTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using ExperienceLevelAPI_basic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperienceLevelAPI_basic.Tests
+{
+    [TestClass]
+    public class ExperienceLevelTest
+    {
+        [TestMethod]
+        public void TestExperienceLevel()
+        {
+            ExperienceLevel hero1 = new ExperienceLevel();
+
+            //Check starting values
+            Assert.AreEqual(0, hero1.GetCurrentExperience());
+            Assert.AreEqual(0, hero1.GetTotalExperience());
+            Assert.AreEqual(0, hero1.GetLevel());
+
+            //Add some xp
+            hero1.AddExperience(20);
+            hero1.AddExperience(45);
+            hero1.AddExperience(13);
+            hero1.SubtractExperience(27);
+
+            Assert.AreEqual(51, hero1.GetCurrentExperience());
+            Assert.AreEqual(51, hero1.GetTotalExperience());
+            Assert.AreEqual(0, hero1.GetLevel());
+
+            //Add xp so we level-up 
+
+            hero1.AddExperience(49);
+            Assert.AreEqual(0, hero1.GetCurrentExperience());
+            Assert.AreEqual(100, hero1.GetTotalExperience());
+            Assert.AreEqual(1, hero1.GetLevel());
+
+            //Add more xp so we level up two levels, then drop and go back up
+            hero1.AddExperience(201);
+            Assert.AreEqual(3, hero1.GetLevel());
+            Assert.AreEqual(1, hero1.GetCurrentExperience());
+            hero1.SubtractExperience(49);
+            Assert.AreEqual(-48, hero1.GetCurrentExperience());
+            Assert.AreEqual(252, hero1.GetTotalExperience());
+            Assert.AreEqual(3, hero1.GetLevel());
+            hero1.AddExperience(48);
+            //level should still be 3
+            Assert.AreEqual(3, hero1.GetLevel());
+
+            //make experience positive again
+            hero1.AddExperience(27);
+
+            //Check where we are
+            Assert.AreEqual(327, hero1.GetTotalExperience()); //Currently, total experience is 327,
+            Assert.AreEqual(27, hero1.GetCurrentExperience());//           current experience is 27, 
+            Assert.AreEqual(3, hero1.GetLevel());             //           and level is 3
+
+            //and some calculations
+            Assert.AreEqual(173, hero1.ExperienceDelta(5));
+            Assert.AreEqual(73, hero1.ExperienceTillLevelUp());
+            Assert.AreEqual(27, hero1.ProgressToNextLevel());
+            Assert.AreEqual(53, hero1.ExperienceToLevel(5347));
+
+            //test ResetExperience()
+
+            hero1.ResetExperience();
+            Assert.AreEqual(0, hero1.GetCurrentExperience(), 0);
+            Assert.AreEqual(3, hero1.GetLevel(), 3);
+
+            //test SetLevel()
+
+            hero1.SetLevel(1);
+            Assert.AreEqual(0, hero1.GetCurrentExperience());
+            Assert.AreEqual(100, hero1.GetTotalExperience());
+            Assert.AreEqual(1, hero1.GetLevel());
+
+        }
+    }
+}
---
+++ ExperienceLevelAPI_basic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExperienceLevelAPI_basic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExperienceLevelAPI_basic.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("681d4db4-a486-418d-92c4-ea6263b83498")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
